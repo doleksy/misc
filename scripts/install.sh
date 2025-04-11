6a44b722953dd4bb568860d1d062aee203b9fcb4
Dave Oleksy <dave@doleksy.com>
2025-04-11 10:53:36 -0500

    Add java, chmod +x

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,3 +7,8 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install btop fzf
 
 sudo apt-get install clang-format clang-tidy cppcheck doxygen g++ gawk git graphviz lcov libpqxx-dev libprotobuf-dev make pkg-config protobuf-compiler tree valgrind vim wget
+
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+sudo apt-get update; sudo apt-get install -y java-21-amazon-corretto-jdk
+
